e8abbfc769266ab373db0277e235e41ef5299aae
Paco Camallonga <paco@imacpilar.icmol.uv.es>
2016-01-27 11:19:51 +0100

    Muestra mensaje cuando todo ha ido bien, es decir, los ficheros son iguales.

--- compare.sh
+++ compare.sh
@@ -1,13 +1,14 @@
 tam1=$(wc -l $1 | awk '{print $1}')
 tam2=$(wc -l $2 | awk '{print $1}')
 
-echo "Size of: "$1 "=" $tam1 
-echo "Size of: "$2 "=" $tam2
-
+echo "Size of: "$1 "=" $tam1 "lines" 
+echo "Size of: "$2 "=" $tam2 "lines"   
+:
 if [ $tam1 -ne $tam2 ] ; then
     echo "ERROR: the files don't have the same size."
 else
     i=1
+    j=0
     paste $1 $2 -d ";" | 
     while read -r LINE || [ -n "$LINE" ]; do
 
@@ -23,14 +24,19 @@ else
 	fi
 
 	diff=$( echo $n1 - $n2 | bc)
-	tol=0.01
-	echo "diff: "$diff" > tol: "$tol
+	tol=0.000001
+#	echo "diff: "$diff" > tol: "$tol
 	if [ $(echo "$diff>$tol" | bc ) -eq 1 ] ; then 
 	    echo $i": NO --> " $diff
-	else
-	    echo "OK --> " $diff
+	    j=$(($j+1))
+#	else
+#	    echo "OK --> " $diff
 	fi
 	i=$(($i+1))
     done
+
+    if [ $j -eq 0 ] ; then
+	echo "*** Everything is OK. ***"
+    fi
 fi
 
